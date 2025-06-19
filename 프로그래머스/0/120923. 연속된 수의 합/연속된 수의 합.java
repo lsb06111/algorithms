477a14e46f684b34e49ad196bfca59da01cb8169
Soobin Lee <58012950+lsb06111@users.noreply.github.com>
2025-06-19 09:01:00 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.04 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.java
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.java
@@ -2,18 +2,23 @@ import java.util.*;
 class Solution {
     public int[] solution(int num, int total) {
         int[] answer = new int[num];
-        for(int i=1000; i >= -100; i--){
-            int sum = 0;
+        
+        int start = num%2==0 ? total/num - num/2+1 : total/num - num/2 ;
+        for(int i=0; i < num; i++)
+            answer[i] = start++;
+        
+//         for(int i=1000; i >= -100; i--){
+//             int sum = 0;
             
-            for(int k=0; k < num; k++){
-                sum += (i-k);
-                answer[k] = (i-k);
-            }
-            if(sum == total){
-                break;
-            }
-        }
-        Arrays.sort(answer);
+//             for(int k=0; k < num; k++){
+//                 sum += (i-k);
+//                 answer[k] = (i-k);
+//             }
+//             if(sum == total){
+//                 break;
+//             }
+//         }
+        // Arrays.sort(answer);
         return answer;
     }
 }
