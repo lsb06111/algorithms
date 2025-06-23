c4cc6a476a3efc5d8209ed769477213ba051577c
Soobin Lee <58012950+lsb06111@users.noreply.github.com>
2025-06-23 13:49:58 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 85.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        int ggochiService = n/10;
+        
+        return n*12000 + (k-ggochiService)*2000;
+    }
+}
