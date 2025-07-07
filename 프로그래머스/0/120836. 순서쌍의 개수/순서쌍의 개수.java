24b102db3a7883a9c6bd04733b67dc04ab42d821
Soobin Lee <58012950+lsb06111@users.noreply.github.com>
2025-07-07 15:05:03 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.16 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -1,22 +1,10 @@
 import java.util.*;
 class Solution {
-    public int gcd(int a, int b) {
-        
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
-    }
-    
-    
-   
     public int solution(int n) {
         int answer = 0;
-        int num = n; // 입력 값
-        int sqrt = (int) Math.sqrt(num); // 100의 제곱근은 10
-        ArrayList<Integer> arr = new ArrayList<>(); // 약수 받을 ArrayList
+        int num = n; 
+        int sqrt = (int) Math.sqrt(num); 
+        ArrayList<Integer> arr = new ArrayList<>();
 
         for(int i = 1; i <= sqrt; i++){
            if(num % i == 0){
