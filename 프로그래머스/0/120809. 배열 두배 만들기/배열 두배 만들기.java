06c55464d1dd395432aaf7570e0d01cebc083999
Soobin Lee <58012950+lsb06111@users.noreply.github.com>
2025-07-09 08:48:38 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.03 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = numbers;
+        for(int i=0; i < answer.length; i++)
+            answer[i] *= 2;
+        return answer;
+    }
+}
