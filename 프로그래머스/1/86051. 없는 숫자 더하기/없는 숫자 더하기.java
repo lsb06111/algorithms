1c29c588498ce920d0479a9358500b4130dd4d35
Soobin Lee <58012950+lsb06111@users.noreply.github.com>
2025-07-15 10:02:37 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.07 ms, Memory: 94.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        HashSet<Integer> set = new HashSet<>();
+        for(int n : numbers)
+            set.add(n);
+        
+        for(int i=0; i < 10; i++)
+            if(!set.contains(i))
+                answer += i;
+        
+        return answer;
+    }
+}
